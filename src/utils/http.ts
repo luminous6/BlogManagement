e30316de7742a35d8298a4713a0db13b1e911303
xiangming <1209852785@qq.com>
2020-04-20 15:34:28 +0800

    chore: 数据请求地址判断

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,13 @@
 import { extend } from "umi-request";
 import { message } from 'antd';
 const request = extend({
-  prefix: '/api',
-  timeout: 1000,
+  prefix:
+    process.env.NODE_ENV === 'production' ? 'http://155.94.151.5:7001' : 'http://127.0.0.1:7001',
+  timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+
+    // 'Content-Type': 'application/json',
   },
   params: {
     // token: "xxx" // 所有请求默认带上 token 参数
